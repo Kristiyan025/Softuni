2f735331c49569b9f388c5edf8395805e5dd8356
Kristyan025 <49957699+Kristyan025@users.noreply.github.com>
2019-07-30 23:49:42 +0300

    Add files via upload

---
+++ 01. Programming Basics - May 2018/Кодове/01.First Steps in Coding/Exs_01 Hello SoftUni/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Exs_01_Hello_SoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello SoftUni");
+
+        }
+    }
+}
---
+++ 01. Programming Basics - May 2018/Кодове/01.First Steps in Coding/Exs_04 Triangle of 55 Stars/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exs_04_Triangle_of_55_Stars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("*");
+            Console.WriteLine("**");
+            Console.WriteLine("***");
+            Console.WriteLine("****");
+            Console.WriteLine("*****");
+            Console.WriteLine("******");
+            Console.WriteLine("*******");
+            Console.WriteLine("********");
+            Console.WriteLine("*********");
+            Console.WriteLine("**********");
+
+        }
+    }
+}
