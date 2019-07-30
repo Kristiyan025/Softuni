04f96ae60f5df9621a8ed661d03628e39cb74d2f
Kristyan025 <49957699+Kristyan025@users.noreply.github.com>
2019-07-31 01:51:18 +0300

    Add files via upload

---
+++ 02. Tech Module - September 2018/01. Кодове/13. Exam Preparation/01. Exam Preparation/ASP-NET-Core-Skeleton/ProjectRider.App/Models/ProjectRiderDbContext.cs
@@ -0,0 +1,19 @@
+﻿namespace ProjectRider.App.Models
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class ProjectRiderDbContext : DbContext
+    {
+        public DbSet<Project> Projects { get; set; }
+
+        public ProjectRiderDbContext(DbContextOptions<ProjectRiderDbContext> options) : base(options)
+        {
+            this.InitializeDatabase();
+        }
+
+        private void InitializeDatabase()
+        {
+            this.Database.EnsureCreated();
+        }
+    }
+}
