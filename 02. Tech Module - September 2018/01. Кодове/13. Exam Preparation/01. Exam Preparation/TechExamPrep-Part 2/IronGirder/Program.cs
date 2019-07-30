e22445f7e5a2e56f86f88f8caa919ad9d5f2b1e0
Kristyan025 <49957699+Kristyan025@users.noreply.github.com>
2019-07-31 01:50:00 +0300

    Add files via upload

---
+++ 02. Tech Module - September 2018/01. Кодове/13. Exam Preparation/01. Exam Preparation/TechExamPrep-Part 2/IronGirder/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace IronGirder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split("|");
+
+            string firstPart = input[0];
+            string secondPart = input[1];
+            string thirdPard = input[2];
+
+            string firstPattern = @"([#$*&%])(?<capitalLetters>[A-Z]+)(\1)";
+            Match firstMatch = Regex.Match(firstPart, firstPattern);
+            string letters = firstMatch.Groups["capitalLetters"].Value;
+
+            for (int index = 0; index < letters.Length; index++)
+            {
+                char currentLetter = letters[index];
+                int asciiCode = currentLetter;
+
+                string secondPattern = $@"{asciiCode}:(?<length>[0-9][0-9])";
+                Match secondMatch = Regex.Match(secondPart, secondPattern);
+                int length = int.Parse(secondMatch.Groups["length"].Value);
+
+                string thirdPattern = $@"(?<=\s|^){currentLetter}[^\s]{{{length}}}(?=\s|$)";
+                Match thirdMatch = Regex.Match(thirdPard, thirdPattern);
+                string word = thirdMatch.ToString();
+
+                Console.WriteLine(word);
+
+            }
+        }
+    }
+}
