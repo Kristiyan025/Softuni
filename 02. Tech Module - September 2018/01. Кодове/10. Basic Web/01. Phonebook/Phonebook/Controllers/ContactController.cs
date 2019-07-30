d0b3b55c046c2f437b810717214b4cf5e3145f47
Kristyan025 <49957699+Kristyan025@users.noreply.github.com>
2019-07-31 01:27:13 +0300

    Add files via upload

---
+++ 02. Tech Module - September 2018/01. Кодове/10. Basic Web/01. Phonebook/Phonebook/Controllers/ContactController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Phonebook.Data.Models;
+using Phonebook.Data;
+
+namespace Phonebook.Controllers
+{
+    public class ContactController : Controller
+    {
+        [HttpPost]
+        public IActionResult Create(Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                DataAccsess.Contacts.Add(contact);
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Name and number are required!";
+            }
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
