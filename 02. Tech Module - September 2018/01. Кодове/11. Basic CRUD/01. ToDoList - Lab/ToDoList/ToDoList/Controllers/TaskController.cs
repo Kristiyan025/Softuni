a13dd01b9129f6a0e8aded78792be4e22ef8d128
Kristyan025 <49957699+Kristyan025@users.noreply.github.com>
2019-07-31 01:37:57 +0300

    Add files via upload

---
+++ 02. Tech Module - September 2018/01. Кодове/11. Basic CRUD/01. ToDoList - Lab/ToDoList/ToDoList/Controllers/TaskController.cs
@@ -0,0 +1,86 @@
+﻿namespace ToDoList.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using System.Linq;
+    using ToDoList.Data;
+    using ToDoList.Models;
+
+    public class TaskController : Controller
+    {
+        public IActionResult Index()
+        {
+            using (var db = new TasksDbContext())
+            {
+                var tasks = db.Tasks.ToList();
+                return View(tasks);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            using (var db = new TasksDbContext())
+            {
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Create(Task newTask)
+        {
+            using(var db = new TasksDbContext())
+            {
+                if (!ModelState.IsValid)
+                {
+                    TempData["ErrorMessage"] = "The title and comment are required!";
+                    return View();
+                }
+                db.Add(newTask);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            using(var db = new TasksDbContext())
+            {
+                Task oldTask = db.Tasks.Find(id);
+                return View(oldTask);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Task oldTask)
+        {
+            using(var db = new TasksDbContext())
+            {
+                db.Tasks.Remove(oldTask);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            using(var db = new TasksDbContext())
+            {
+                var task = db.Tasks.Find(id);
+                return View(task);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Task newTask)
+        {
+            using(var db = new TasksDbContext())
+            {
+                db.Tasks.Update(newTask);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
---
+++ 02. Tech Module - September 2018/01. Кодове/11. Basic CRUD/01. ToDoList - Lab/ToDoList/ToDoList/Data/TasksDbContext.cs
@@ -0,0 +1,20 @@
+﻿namespace ToDoList.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using ToDoList.Models;
+
+    public class TasksDbContext : DbContext
+    {
+        private const string ConnectionString = @"Server=.\SQLEXPRESS01;Database=ToDoListDb; Integrated Security=true";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString);
+        }
+
+        public virtual DbSet<Task> Tasks { get; set; }
+
+
+
+    }
+}
